877ceba0060247410a1cff8875c578b7b52f41af
chinchana k <chinchanak640@gmail.com>
2025-04-05 19:43:34 +0530

    Update prmg6.py

--- lab7/prmg6.py
+++ lab7/prmg6.py
@@ -7,6 +7,8 @@ array_2d = array_1d.reshape(2,3)
 print("\nReshape to 2D Array (2*3):")
 print(array_2d)
 
+print("\nelement at position(1,2):",array_2d[1,2])
+
 print("\nModified array (after changing element at position(0,1)to 10):")
 print(array_2d)
 
